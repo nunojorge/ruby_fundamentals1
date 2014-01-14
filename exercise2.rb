9a990ecda9ed5f95fb2cd4d35c2dab101eb10ab7
nunojorge <nujo@hotmail.com>
2014-01-14 14:02:52 -0500

    final version of 2

---
+++ exercise2.rb
@@ -0,0 +1,11 @@
+# 1. Calculate a good 15% tip for a 55 dollar meal
+puts "A good tip would be #{55*0.15} dollars"
+
+# 2. Converting an integer to a string and adding it to another string
+puts 34.to_s+" was converted to a string and added to this!"
+
+# 3. String interpolation example
+puts "45628 multiplied by 7839 is #{45628 * 7839}"
+
+# 4. This expression should be true
+ puts (true && false) || (false && true) || !(false && false) 
